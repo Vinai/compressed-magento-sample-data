fd13b9d6d526ff2d21e48d95907010329a7a447c
Vinai Kopp <vinai@netzarbeiter.com>
2014-11-11 10:18:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Compressed Magento 1.9.0.0 Sample Data
+
+Its kind of ridiculous having a sample data package that is 317MB in size, and since I couldn't find
+a good alternative source, so I decided to hack together a little script to compress the hell out of the images and the MP3 files
+in the sample data archive, so it can be downloaded and used more easily.
+
+With a image quality setting of 50% and resampling all mp3 files with a bitrate of 48, the size of the sample data is down to 66M.
+
+This is okay for my purposes.
+
+The compression script is intended to run on OS X. Feel free to use and modify it, but be aware that you are doing so at your own risk!
+
---
+++ src/compress-sample-data.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#
+# This script agressivly compresses the magento sample data images and mp3 files
+# Use at your own risk!
+#
+# It is a quick hack, intended only to run on OSX with the following dependencies:
+# - ImageMagick (that is, the convert command)
+# - ImageOptim
+# - lame
+# - grealpath
+# - curl (only for downloading the sample data)
+#
+# (c) 2014 Vinai Kopp <vinai@netzarbeiter.com>
+# 
+
+TARGET_MP3_BITRATE=48
+TARGET_IMAGE_QUALITY_PERCENTAGE=50
+
+
+if [ -z "$1" ]; then
+    echo "No sample data specified."
+    read -r -p "Do you want to download the 1.9 sample data? [yN] "
+    [[ "$REPLY" = [Yy] ]] && download=http://www.magentocommerce.com/downloads/assets/1.9.0.0/magento-sample-data-1.9.0.0.tar.bz2
+
+elif echo "$1" | grep -q '^https\?:'; then
+    download="$1"
+fi
+
+if [ -n "$download" ]; then
+    echo "Downloading $download"
+    curl -O "$download"
+    SOURCE_ARCHIVE="$(grealpath "$(basename "$download")")"
+
+elif [ -n "$1" ]; then
+    SOURCE_ARCHIVE="$(grealpath "$1")"
+fi
+
+[ ! -e "$SOURCE_ARCHIVE" ] && {
+    echo -e "Usage:\n$0 magento-sample-data-1.x.x.x.tar.bz2"
+    exit 2
+}
+echo "Using sample data $SOURCE_ARCHIVE"
+
+ORIG_SIZE=$(du -sh "$SOURCE_ARCHIVE" | awk '{ print $1 }')
+SAMPLE_DATA_DIR=$(tar -tvzf "$SOURCE_ARCHIVE" | head -1 | awk '{ print $9 }' | xargs basename)
+
+WORK_DIR="./tmp-work-dir"
+echo "Creating temporary working dir $WORK_DIR"
+mkdir "$WORK_DIR" && cd "$WORK_DIR"
+echo "Extracting sample data..."
+tar -xzf "$SOURCE_ARCHIVE"
+
+echo "Removing resized images cache files"
+rm -rf "$SAMPLE_DATA_DIR"/media/catalog/product/cache/*
+echo "Compressing images..."
+find "$SAMPLE_DATA_DIR" -type f \( -iname '*.jpg' -o -iname '*.png' -o -iname '*.gif' \) -exec convert -quality $TARGET_IMAGE_QUALITY_PERCENTAGE "{}" "{}" \; -exec open -a ImageOptim.app "{}" \;
+echo "Compressing mp3 files..."
+find "$SAMPLE_DATA_DIR" -type f -iname '*.mp3' -exec lame --silent -b $TARGET_MP3_BITRATE "{}" "{}.out" \; -exec mv "{}.out" "{}" \;
+
+echo "Building new sample data archive..."
+tar -cjf "../$SAMPLE_DATA_DIR.tbz" "$SAMPLE_DATA_DIR"
+cd .. # get out of the tmp-work-dir
+rm -r "$WORK_DIR"
+
+NEW_SIZE=$(du -sh "$SAMPLE_DATA_DIR.tbz" | awk '{ print $1 }');
+echo "New compressed sample data archive: $SAMPLE_DATA_DIR.tbz"
+echo "Original size:   $ORIG_SIZE"
+echo "Compressed size: $NEW_SIZE"
+
